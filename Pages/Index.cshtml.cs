e979f438a5af1e386caa50255a842ef738e42c3d
rashadmohamedr <51525838+rashadmohamedr@users.noreply.github.com>
2024-03-06 19:39:01 +0200

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<string> Tasks { get; set; } = new List<string>();
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("/Task", new { Tasks = this.Tasks });
+        }
+    }
+}
---
+++ Pages/Task.cshtml.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Pages
+{
+    public class TaskModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public List<string> Tasks { get; set; } = new List<string>();
+        public IActionResult OnPost()
+        {
+            string text = Request.Form["TaskText"];
+            Tasks.Add(text);
+
+            Console.WriteLine("------");
+            foreach (string Task in Tasks)
+            {
+            Console.WriteLine(Task);
+            }
+
+            Console.WriteLine("------^");
+            return RedirectToPage("/Index", new { Tasks =this.Tasks });
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
